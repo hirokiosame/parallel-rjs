1796536f33e3c38758ee3574434749ce27c86d64
Hiroki Osame <hiroki@weebly.com>
2016-04-11 20:39:24 -0700

    typo

--- src/utils/log.js
+++ src/utils/log.js
@@ -1,6 +1,7 @@
+'use strict';
 
 export default function log(...args){
-	let date = new Date();
+	let date = new Date(),
 		time = [date.getHours(), date.getMinutes(), date.getSeconds()]
 			.map(t => t < 10 ? '0' + t : t).join(':');
 
